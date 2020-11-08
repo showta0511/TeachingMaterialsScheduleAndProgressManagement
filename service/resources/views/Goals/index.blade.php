55588aa08bd7e6c92a45b89a749226a4861e7563
showta0511 <mishow0511@gmail.com>
2020-11-08 22:41:17 +0900

    home画面デザイン完成#2

--- service/resources/views/Goals/index.blade.php
+++ service/resources/views/Goals/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
 
     <div class="row">
-    @if(empty($goal))
+        @if(empty($goal))
         <div class="col-lg-8 col-lg-offset-2 centered">
             <h1>ようこそ！</h1>
             <p>さっそく理想の入力しよう！</p>
@@ -13,18 +13,22 @@
     </div>
     @endif
     @if(!empty($goal))
-    <div class="container">
-        <a class="nav-link" href="{{route('goal.show',['goal'=>$goal->id])}}">
+    <div class="container mt-5">
+
         <div class="card">
             <div class="card-header">
-                なりたい自分
+                <a class="nav-link" href="{{route('goal.show',['goal'=>$goal->id])}}">
+                <strong>なりたい自分</strong>
             </div>
             <div class="card-body ">
+
                 <h1>{{$goal->title}}</h1>
                 <p class="card-text">動機：{{$goal->motive}}</p>
             </div>
+            </a>
+
         </div>
-        </a>
+
     </div>
     <div class="container">
         @if ($errors->any())
@@ -32,31 +36,41 @@
             <ul>
                 <br>
                 @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
+                <li>{{ $error }}</li>
                 @endforeach
             </ul>
         </div>
         @endif
         @if(!empty($for_goals))
-            <div class="list-group pt-5">
-                <button class="list-group-item list-group-item-action active" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                なりたい自分に必要な要素
+        <div class="title-wrapper">
+            <span class="title title-m">
+                <strong>[その為にできるようにならないといけない事]</strong>
+            </span>
+            <span class="title">
+                <button style="font-size:10px;" class="btn btn-primary title-btn" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    作成
                 </button>
-                <div class="collapse" id="collapseExample">
-                    <div class="card card-body">
-                        <form action="{{route('for_goal.store')}}" method="post">
-                            @csrf
-                            <input type="hidden" name="goal_id" value="{{$goal->id}}">
-                            <label for="title">必要な要素</label>
-                            <input id="title" name="title" type="text" value="{{old('title')}}">
-                            <input type="submit" value="作成する">
-                        </form>
-                    </div>
+            </span>
+        </div>
+
+        <div class="list-group mb-5">
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    <form action="{{route('for_goal.store')}}" method="post">
+                        @csrf
+                        <input type="hidden" name="goal_id" value="{{$goal->id}}">
+                        <label for="title">その為にできるようにならないといけない事</label>
+                        <input id="title" name="title" type="text" value="{{old('title')}}">
+                        <input type="submit" value="作成する">
+                    </form>
                 </div>
-                @foreach($for_goals as $for_goal)
-                <a class="list-group-item list-group-item-action" href="{{route('for_goal.show',['for_goal'=>$for_goal->id])}}">{{$for_goal->title}}</a>
-                @endforeach
             </div>
+            @foreach($for_goals as $for_goal)
+            <a class="list-group-item list-group-item-action" href="{{route('for_goal.show',['for_goal'=>$for_goal->id])}}">
+                <h2>{{$for_goal->title}}</h2>
+            </a>
+            @endforeach
+        </div>
 
         @endif
     </div>
