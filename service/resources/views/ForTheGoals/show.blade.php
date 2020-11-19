adfc5afc4f1407bc6d0ce24f830c4c5bfe92ef48
showta0511 <mishow0511@gmail.com>
2020-11-19 11:51:59 +0900

    for_goalの編集、削除ボタン表示変更#2

--- service/resources/views/ForTheGoals/show.blade.php
+++ service/resources/views/ForTheGoals/show.blade.php
@@ -4,18 +4,37 @@
 <div class="container">
     <div class="container">
         <div class="row">
-            <div class="col-sm-10">
-                <strong>目標達成に必要な要素</strong>
-                <h1>{{$for_goal->title}}</h1>
+            <div class="container content-card mt-5">
+                <h1 class="content-title mt-3">目標達成に必要な要素
+                    <a href="#" data-toggle="collapse" data-target="#collapseExample" aria-expanded="true" aria-controls="collapseExample" class="mb-3" style="width: 20px; height:18px; padding:0 0 9px 6px; box-shadow: none; margin-left:20px;">
+                        <img style="width: 25px; height: 25px;" class="" src="/TeachingMaterialsScheduleAndProgressManagement/service/resources/images/レンチ＆ドライバーの設定アイコン.png" alt="">
+                            <li style="list-style: none; float:right;" id="collapseExample" class="setting_li_1">
+                                <a href="{{route('for_goal.del_conform',['del'=>$for_goal->id])}}" class="btn delete-btn btn-link mt-1">削除</a>
+                            </li>
+                            <li style="list-style: none; float:right;" id="collapseExample" class="setting_li_2">
+                                <a href="{{route('for_goal.edit',['edit'=>$for_goal->id])}}" class="btn edit-btn btn-link mt-1">編集</a>
+                            </li>
+                    </a>
+                </h1>
+
+                <div class="card-body ">
+                    <h1>{{$for_goal->title}}
+
+
+                        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1" style="width: 200px; border-color: #30c8d6;">
+                            <ul class="" style="margin:0 auto; list-style: none;">
+                                <li style="list-style: none; float:left;">
+                                    <a href="{{route('for_goal.edit',['edit'=>$for_goal->id])}}" class="btn edit-btn btn-link">編集</a>
+                                </li>
+                                <li style="list-style: none; float:left;" class="ml-3">
+                                    <a href="{{route('for_goal.del_conform',['del'=>$for_goal->id])}}" class="btn delete-btn btn-link">削除</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </h1>
+                </div>
             </div>
-            <ul class="li-link" style="list-style: none; padding-left:520px;">
-                <li style="list-style: none;">
-                    <a href="{{route('for_goal.edit',['edit'=>$for_goal->id])}}" class="btn edit-btn btn-link">編集</a>
-                </li>
-                <li style="list-style: none;" class="ml-3">
-                    <a href="{{route('for_goal.del_conform',['del'=>$for_goal->id])}}" class="btn delete-btn btn-link">削除</a>
-                </li>
-            </ul>
         </div>
         <div class="container">
             <table>
@@ -28,6 +47,7 @@
                         <th><a style="border-color: none; padding:3;" class="btn btn-primary title-btn" href="{{route('setting_schedule.show',['setting_schedule'=>$setting_schedule->id])}}">設定内容</a></th>
                         @endif
                     </tr>
+                    <th><a style="border-color: none; padding:3;" btn edit-btn btn-link mb-3 collapsed href="{{route('setting_schedule.show',['setting_schedule'=>$setting_schedule->id])}}">設定内容</a></th>
                     <tr>
                         <th>日程</th>
                         <th>ページ</th>
